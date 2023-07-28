d3873e9c8ddf7780ce216932c05d69f3d56f20c2
rhellstrom <rasmus.hellstrom@ha.ax>
2023-07-28 09:04:33 +0300

    Rewrote get_resource() into get_resources() that tries to update an existing vector of Server instead of creating it

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -3,17 +3,31 @@ mod requests;
 
 use server::Server;
 use reqwest::Error;
-use crate::requests::get_resource;
+use crate::requests::get_resources;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let mut server_endpoints: Vec<String> = vec![];
     let url = "http://localhost:3000/resources".to_string();
-    server_endpoints.push(url); // Replace with the appropriate port and path if needed
+    let url2 = "faulty.endpoint:3000/resources".to_string();
+    server_endpoints.push(url);
+    server_endpoints.push(url2);
 
+    let mut servers: Vec<Server> = vec![];
+
+    //Create a Server for each endpoint we wish to track and populate with default values until we call refresh
+    for endpoint in server_endpoints {
+        servers.push(Server {
+            endpoint,
+            ..Default::default()
+        })
+    }
+    println!("{:?}", servers);
+    println!();
+    println!();
     let client = reqwest::Client::new();
 
-    let servers: Vec<Server> = get_resource(server_endpoints, client).await;
+    get_resources(&mut servers, client).await;
 
     println!("{:?}", servers);
     Ok(())
--- dashboard/src/requests.rs
+++ dashboard/src/requests.rs
@@ -1,37 +1,19 @@
 use crate::server::Server;
 use reqwest::{Client, StatusCode};
 
-//Maybe we should init each struct as default and run them all through a refresh function instead?
-//Saving up on a function
 
-/// Requests data from each server endpoint and deserializes it into a Server.
-/// If an endpoint returns a status other than 200 we add a Server with default values instead.
-/// Returns a vector with each element representing a server.
-pub async fn get_resource(endpoints: Vec<String>, client: Client) -> Vec<Server> {
-    let mut servers: Vec<Server> = vec![];
-
-    for url in endpoints{
-        let response = client.get(&url).send().await.unwrap();
-        match response.status() {
-            StatusCode::OK => {
+/// Iterates through the vector of Server and makes a GET request to each endpoint
+/// and updates the struct if we got a status code 200 in the response.
+/// Otherwise we silently fail and try again next iteration
+pub async fn get_resources(servers: &mut Vec<Server>, client: Client) {
+    for server in servers {
+        let endpoint = server.endpoint.clone();
+        if let Ok(response) = client.get(&endpoint).send().await {
+            if response.status() == StatusCode::OK {
                 let body = response.text().await.unwrap();
-                let mut server_resources: Server = serde_json::from_str(&body).unwrap();
-                server_resources.endpoint = url;
-                servers.push(server_resources);
-            }
-            _ => {
-                servers.push(Server {
-                    endpoint: url,
-                    ..Default::default()
-                });
+                *server = serde_json::from_str(&body).unwrap();
+                server.endpoint = endpoint;
             }
         }
     }
-    servers
 }
-
-/*
-//Acquire lock, fetch resource and update field of struct for each server
-pub async fn refresh_resources(servers: Vec<Server>, interval: u64, client: Client){
-
-} */
