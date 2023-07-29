26361f918a673f9fb19725c43c1f4fe083661551
rhellstrom <rasmus.hellstrom@ha.ax>
2023-07-29 21:06:19 +0300

    Added a hello world ratatui example to get started

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -1,16 +1,19 @@
 mod server;
 mod requests;
+mod terminal;
 
 use std::sync::{Arc};
 use std::thread::sleep;
 use std::time::Duration;
+use anyhow::{Context, Result};
 use reqwest::Error;
 use crate::requests::{refresh_servers};
 use crate::server::init_with_endpoint;
 use tokio::sync::Mutex;
+use crate::terminal::{restore_terminal, run, setup_terminal};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
     let mut server_endpoints: Vec<String> = vec![];
     let url = "http://localhost:3000/resources".to_string();
     let url2 = "faulty.endpoint:3000/resources".to_string();
@@ -28,10 +31,15 @@ async fn main() -> Result<(), Error> {
         let rt = tokio::runtime::Runtime::new().unwrap();
         rt.block_on(refresh_servers(servers_clone, 4))
     });
-
+    /*
     loop {
         sleep(Duration::from_secs(4));
         println!("{:?}", servers.lock().await);
         println!();
-    }
+    } */
+
+    let mut terminal = setup_terminal().context("setup failed")?;
+    run(&mut terminal).context("app loop failed")?;
+    restore_terminal(&mut terminal).context("restore terminal failed")?;
+    Ok(())
 }
