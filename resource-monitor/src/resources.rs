2b4ed5eb2ceb1b622eb87a860fea387c8dd3531c
Rasmus <rasmus.hellstrom@ha.ax>
2023-10-04 13:50:52 +0300

    Added bytes received and transmitted from networks interfaces

--- resource-monitor/src/resources.rs
+++ resource-monitor/src/resources.rs
@@ -1,4 +1,4 @@
-use sysinfo::{CpuExt, DiskExt, RefreshKind, System, SystemExt};
+use sysinfo::{CpuExt, DiskExt, NetworkExt, RefreshKind, System, SystemExt};
 use serde::{Serialize};
 
 
@@ -23,6 +23,8 @@ pub struct Resources {
     load_avg_one: f64,
     load_avg_five: f64,
     load_avg_fifteen: f64,
+    bytes_received: u64,
+    bytes_transmitted: u64,
 
     #[serde(skip_serializing)]
     system_struct: System,
@@ -57,28 +59,28 @@ impl Resources {
             load_avg_one: sys.load_average().one,
             load_avg_five: sys.load_average().five,
             load_avg_fifteen: sys.load_average().fifteen,
+            bytes_received: get_total_received(&sys),
+            bytes_transmitted: get_total_transmitted(&sys),
             system_struct: sys,
 
         }
     }
 
-    /// Refreshes the CPU, memory and disk usage
+    /// Refreshes the desired fields of our Resources struct
     pub(crate) fn refresh(&mut self) {
         self.system_struct.refresh_all();
-
         self.cpu_usage = self.system_struct.global_cpu_info().cpu_usage();
-
-
         self.used_memory = self.system_struct.used_memory();
         self.available_space = disk_total_usage(&mut self.system_struct).1;
         self.used_swap = self.system_struct.used_swap();
         self.cpu_load_per_core = get_cpu_load_per_core(&self.system_struct);
         self.disk_available = get_disks_available(&mut self.system_struct);
         self.uptime = self.system_struct.uptime();
-
         self.load_avg_one = self.system_struct.load_average().one;
         self.load_avg_five = self.system_struct.load_average().five;
         self.load_avg_fifteen = self.system_struct.load_average().fifteen;
+        self.bytes_transmitted = get_total_transmitted(&self.system_struct);
+        self.bytes_received = get_total_received(&self.system_struct);
     }
 
     pub fn serialize(&self) -> String {
@@ -133,15 +135,33 @@ fn get_cpu_load_per_core(sys: &System) -> Vec<f32> {
     load_per_core
 }
 
+///Iterates over interfaces and returns total bytes received
+fn get_total_received(sys: &System) -> u64 {
+    let networks = sys.networks();
+    let mut total = 0;
+    for (_, network) in networks {
+        total += network.total_received();
+    }
+    total
+}
+
+///Iterates over interfaces and returns total bytes transmitted
+fn get_total_transmitted(sys: &System) -> u64 {
+    let networks = sys.networks();
+    let mut total = 0;
+    for (_, network) in networks {
+        total += network.total_transmitted();
+    }
+    total
+}
+
 /// Returns a System struct using System::new_with_specifics and fills it with information using refresh.all()
 fn get_system() -> System {
     let mut sys = System::new_with_specifics(
         RefreshKind::everything()
             .without_components()
-            .without_networks()
             .without_processes()
             .without_users_list()
-            .without_networks_list()
     );
     sys.refresh_all();
     sys
