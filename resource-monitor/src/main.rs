36290e637784becbdfa23885f29f60647cf19792
Rasmus <rasmus.hellstrom@ha.ax>
2023-10-30 10:38:58 +0200

    Switched to tokio mutex instead of std

--- resource-monitor/src/main.rs
+++ resource-monitor/src/main.rs
@@ -4,26 +4,25 @@ mod args;
 use crate::resources::{Resources};
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
 use std::time::Duration;
 use axum::http::header::CONTENT_TYPE;
 use clap::Parser;
+use tokio::sync::Mutex;
 use tokio::time::sleep;
 use args::Args;
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
+    let server_resources = Arc::new(Mutex::new(Resources::new()));
 
-    //Initialize resources and wrap in an Arc and Mutex to be shared across threads
-    let resource_mutex = Arc::new(Mutex::new(Resources::new()));
-    tokio::spawn(refresh_loop(Arc::clone(&resource_mutex), args.update_frequency));
+    tokio::spawn(refresh_loop(Arc::clone(&server_resources), args.update_frequency));
 
     let app = Router::new().route("/resources", get(move || {
-        //Clone arc to increase reference count
-        let resource_mutex = Arc::clone(&resource_mutex);
+        let resource_mutex = Arc::clone(&server_resources);
         async move {
-            let resource = resource_mutex.lock().unwrap();
+            let resource = resource_mutex.lock().await;
             let json_response = resource.serialize(); //Content-Type JSON
             axum::http::Response::builder()
                 .header(CONTENT_TYPE, "application/json")
@@ -42,7 +41,7 @@ async fn main() {
 async fn refresh_loop(resources: Arc<Mutex<Resources>>, update_frequency: u64) {
     loop {
         sleep(Duration::from_millis(update_frequency)).await;
-        let mut resource = resources.lock().unwrap();
+        let mut resource = resources.lock().await;
         resource.refresh();
     }
 }
